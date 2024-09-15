38f0ad2f83487ed35c71e780153128f0e83afe52
Daniel Jin <mrpickles123115@gmail.com>
2024-09-15 16:40:31 -0600

    have Tree class play nice with dictionaries from the database

--- src/tree.py
+++ src/tree.py
@@ -11,13 +11,22 @@ class Tree:
         self.location = self.parseLocation()
         try:
             self.name = self.json['owner']
-            self.type = self.json['type']
+
+            # why is it breed in database but type in the schema :sob:
+            try:
+                self.type = self.json['type']
+            except KeyError:
+                self.type = self.json['breed']
 
             self.images = self.json['images']
             if not isinstance(self.images, list):
                 raise TypeError("Images should be an instance of the list type")
 
-            self.date = Date(self.json['date'])
+            # :sob:
+            try:
+                self.date = Date(self.json['date'])
+            except KeyError:
+                self.date = Date(self.json['date_planted'])
 
             try:
                 self.visits = int(self.json['visits'])
@@ -27,11 +36,25 @@ class Tree:
             if self.visits < 0:
                 raise ValueError(f"visits ({self.visits}) field should not be negative")
 
-        except KeyError:
-            raise ValueError("One or more required fields missing")
+        except KeyError as e:
+            raise ValueError(f"One or more required fields missing {e}")
 
         #Everything is good, assign the tree a UUID if it exists, otherwise keep the old one
         self.uid = uid if uid else str(uuid.uuid4())
+
+    def toJson(self):
+        json =  {
+                'tree_id' : self.uid,
+                'owner' : self.name,
+                'date' : self.date.toStr(),
+                'type' : self.type,
+                'images' : self.images,
+                'visits' : self.visits
+                }
+        if self.location:
+            json['location'] = [self.location.latitude,
+                                self.location.longitude]
+        return json
 
     """Check for Location field in JSON, returns either None if not found or two tuple of floats if field exists. Performs verification of data"""
     def parseLocation(self):
@@ -45,19 +68,32 @@ class Tree:
         # B: The values of these floats are valid longitude and latitude values
 
         location = self.json.get("location")
-        if not {"latitude", "longitude"}.issubset(location.keys()):
-            raise ValueError("Location data does not contain latitude and longitude keys")
+        if location is None:
+            return None
 
-        try:
-            lat, lon = float(location.get("latitude")), float(location.get("longitude"))
-        except ValueError:
-            raise ValueError(f"Location data {location} are not floats")
+        # Handle special point
+        if isinstance(location, bytes):
+            try:
+                lat, lon = self.parsePoint(location)
+            except ValueError as e:
+                raise e
+        else:
+            if not {"latitude", "longitude"}.issubset(location.keys()):
+                    raise ValueError("Location data does not contain latitude and longitude keys")
+            try:
+                lat, lon = float(location.get("latitude")), float(location.get("longitude"))
+            except ValueError:
+                raise ValueError(f"Location data {location} are not floats")
 
         if ((abs(lat) - 90.0 > TOLERANCE) or
             (abs(lon) - 180.0 > TOLERANCE)):
             raise ValueError(f"Invalid location data {lat} {lon}")
 
         return Location(lat=lat, long=lon)
+
+    # TODO: figure out how to parse the sql point format
+    def parsePoint(self, p):
+        return (0.0, 0.0)
     
     def save(self):
         execute_sql("DELETE FROM tree WHERE tree_id=%(uid)s", {"uid": self.uid})
@@ -80,14 +116,24 @@ class Location:
         self.latitude = lat
         self.longitude = long
 
+
 class Date:
     def __init__(self, parsing):
-        if len(parsing) != 10:
-            raise ValueError("Date received in incorrect format (invalid length). Expected `dd/mm/yyyy`")
 
-        self.day = parsing[0:2]
-        self.month = parsing[3:5]
-        self.year = parsing[6:10]
+        if isinstance(parsing, date):
+            self.day = parsing.day
+            self.month = parsing.month
+            self.year = parsing.year
 
-        if not (self.day.isdigit() and self.month.isdigit() and self.year.isdigit()):
-            raise ValueError("Date received in incorrect format. Expected `dd/mm/yyyy`")
+        else:
+            if len(parsing) != 10:
+                raise ValueError("Date received in incorrect format (invalid length). Expected `dd/mm/yyyy`")
+
+            self.day = parsing[0:2]
+            self.month = parsing[3:5]
+            self.year = parsing[6:10]
+
+            if not (self.day.isdigit() and self.month.isdigit() and self.year.isdigit()):
+                raise ValueError("Date received in incorrect format. Expected `dd/mm/yyyy`")
+    def toStr(self):
+        return f"{self.day}/{self.month}/{self.year}"
